a9f72389a072fc04f136f067312d684c031e5ac3
Luis M Torres <luis.torres@dixper.gg>
2024-05-26 20:37:26 +0200

    feat: features images

--- src/app/shared/models/features.model.ts
+++ src/app/shared/models/features.model.ts
@@ -3,43 +3,43 @@ import { Feature } from '../interfaces/feature.interface';
 export const FeaturesModel: Record<string, Feature[]> = {
   en: [
     {
-      img: 'https://landing-open-rewards.web.app/assets/features/tiers.webp',
+      img: '/assets/images/f-1.png',
       title: 'Tiers',
       description: `Assign specific benefits to each tier, such as permanent discounts and special point multipliers.`,
       soon: false,
     },
     {
-      img: 'https://landing-open-rewards.web.app/assets/features/points.webp',
+      img: '/assets/images/f-2.png',
       title: 'Points',
       description: `Configure custom events and give different amount of points for activities unique to your business.      `,
       soon: false,
     },
     {
-      img: 'https://landing-open-rewards.web.app/assets/features/achievements.webp',
+      img: '/assets/images/f-3.png',
       title: 'Achievements',
       description: `Set challenges with goals based on transactional data, customer behavior or activity streaks. Create simple milestones or multi-dimensional achievements composed of several rules.      `,
       soon: false,
     },
     {
-      img: 'https://landing-open-rewards.web.app/assets/features/notifications.webp',
+      img: '/assets/images/f-4.png',
       title: 'Webhooks',
       description: `The new webhook mechanism is based on subscriptions to individual events occurring in the system.      `,
       soon: false,
     },
     {
-      img: 'https://landing-open-rewards.web.app/assets/features/battle-pass.webp',
+      img: '/assets/images/f-5.png',
       title: 'Battle pass',
       description: `Set challenges with goals based on transactional data, customer behavior or activity streaks. Create simple milestones or multi-dimensional achievements composed of several rules.      `,
       soon: true,
     },
     {
-      img: 'https://landing-open-rewards.web.app/assets/features/coupons.webp',
+      img: '/assets/images/f-6.png',
       title: 'Coupons',
       description: `Choose between a selection of coupon redemption rules or allow users to redeem coupons from the rewards catalog or in-store.      `,
       soon: true,
     },
     {
-      img: 'https://landing-open-rewards.web.app/assets/features/referrals.webp',
+      img: '/assets/images/f-7.png',
       title: 'Referrals',
       description: `Referrals are a powerful promotional tactic, helping businesses acquire customers more easily and cost-effectively.      `,
       soon: true,
--- src/app/shared/models/how-it-works.model.ts
+++ src/app/shared/models/how-it-works.model.ts
@@ -3,17 +3,17 @@ import { HowItWorks } from '../interfaces/how-it-works.interface';
 export const HowItWorksModel: Record<string, HowItWorks[]> = {
   en: [
     {
-      img: 'https://turso.tech/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FScalableImage.53076c27.png&w=1920&q=75',
+      img: '/assets/images/feature-1.png',
       title: 'Any shape of achievements',
       description: `Utilize gamification software to create achievements that align with business objectives like enhancing purchase frequency or customer lifetime value. Establish challenges based on transactional data, customer behavior, or activity streaks. Design both simple milestones and complex, multi-dimensional achievements with multiple rules.`,
     },
     {
-      img: 'https://turso.tech/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftalk-to-us-background.59f7e65f.png&w=2048&q=75',
+      img: '/assets/images/feature-2.png',
       title: 'Campaigns',
       description: `Enable customers to join achievement-based campaigns, offering benefits like points and rewards, and configure campaigns that reward both milestone completion and full challenge achievement.`,
     },
     {
-      img: 'https://turso.tech/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ffast.9a45e10b.png&w=640&q=75',
+      img: '/assets/images/feature-3.png',
       title: 'Data driven',
       description: `Track and analyze the effectiveness of achievement-based campaigns to enhance loyalty marketing and business outcomes.`,
     },
